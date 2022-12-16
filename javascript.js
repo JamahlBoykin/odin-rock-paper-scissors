748f4d10633418971a7e4c280d9642b86f8c9130
JamahlBoykin <118788306+JamahlBoykin@users.noreply.github.com>
2022-12-15 19:56:55 -0800

    Create the playRound function
    This function returns the outcome of a game of rock paper scissors after
    calling getComputerChoice and getPlayerChoice to pick one of the options
    for each side.

--- javascript.js
+++ javascript.js
@@ -1,6 +1,6 @@
 function getComputerChoice() {
     const randNum = Math.floor(Math.random() * 3);
-    console.log(randNum);
+
     if (randNum === 0) {
         return "Rock";
     }
@@ -17,4 +17,38 @@ function getComputerChoice() {
 
 function getPlayerChoice() {
     return prompt("Please enter either Rock, Paper, or Scissors.");
+}
+
+function playRound(playerSelection, computerSelection) {
+    const computer = computerSelection;
+    const player = playerSelection.toLowerCase();
+
+    if (computer.toLowerCase() === player) {
+        return "It's a tie!";
+    }
+    
+    if (computer === "Rock") {
+        if (player === "paper") {
+            return "You win! Paper beats Rock.";
+        }
+        else {
+            return "You lose! Rock beats Scissors.";
+        }
+    }
+    else if (computer === "Paper") {
+        if (player === "scissors") {
+            return "You win! Scissors beats Paper.";
+        }
+        else {
+            return "You lose! Paper beats Rock.";
+        }
+    }
+    else if (computer === "Scissors") {
+        if (player === "rock") {
+            return "You win! Rock beats Scissors.";
+        }
+        else {
+            return "You lose! Scissors beats paper.";
+        }
+    }
 }
