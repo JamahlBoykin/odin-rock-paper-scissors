0e697795cb147a6052b7c59fc138ad46919a6401
JamahlBoykin <118788306+JamahlBoykin@users.noreply.github.com>
2022-12-15 20:39:33 -0800

    Create the game function
    This function calls playRound until either the player or the computer
    wins a best of five and logs the outcome.

--- javascript.js
+++ javascript.js
@@ -1,54 +1,76 @@
 function getComputerChoice() {
-    const randNum = Math.floor(Math.random() * 3);
+  const randNum = Math.floor(Math.random() * 3);
 
-    if (randNum === 0) {
-        return "Rock";
-    }
-    else if (randNum === 1) {
-        return "Paper";
-    }
-    else if (randNum === 2) {
-        return "Scissors";
-    }
-    else {
-        return "Error"
-    }
+  if (randNum === 0) {
+    return "Rock";
+  } else if (randNum === 1) {
+    return "Paper";
+  } else if (randNum === 2) {
+    return "Scissors";
+  } else {
+    return "Error";
+  }
 }
 
 function getPlayerChoice() {
-    return prompt("Please enter either Rock, Paper, or Scissors.");
+  return prompt("Please enter either Rock, Paper, or Scissors.");
 }
 
 function playRound(playerSelection, computerSelection) {
-    const computer = computerSelection;
-    const player = playerSelection.toLowerCase();
+  const computer = computerSelection;
+  const player = playerSelection.toLowerCase();
 
-    if (computer.toLowerCase() === player) {
-        return "It's a tie!";
+  if (computer.toLowerCase() === player) {
+    return "It's a tie!";
+  }
+
+  if (computer === "Rock") {
+    if (player === "paper") {
+      return "You win! Paper beats Rock.";
+    } else {
+      return "You lose! Rock beats Scissors.";
     }
-    
-    if (computer === "Rock") {
-        if (player === "paper") {
-            return "You win! Paper beats Rock.";
-        }
-        else {
-            return "You lose! Rock beats Scissors.";
-        }
+  } else if (computer === "Paper") {
+    if (player === "scissors") {
+      return "You win! Scissors beats Paper.";
+    } else {
+      return "You lose! Paper beats Rock.";
     }
-    else if (computer === "Paper") {
-        if (player === "scissors") {
-            return "You win! Scissors beats Paper.";
-        }
-        else {
-            return "You lose! Paper beats Rock.";
-        }
+  } else if (computer === "Scissors") {
+    if (player === "rock") {
+      return "You win! Rock beats Scissors.";
+    } else {
+      return "You lose! Scissors beats paper.";
     }
-    else if (computer === "Scissors") {
-        if (player === "rock") {
-            return "You win! Rock beats Scissors.";
-        }
-        else {
-            return "You lose! Scissors beats paper.";
-        }
+  }
+}
+
+function game() {
+  let playerScore = 0;
+  let computerScore = 0;
+
+  for (let i = 0; i < 5; i++) {
+    const outcome = playRound(getPlayerChoice(), getComputerChoice());
+    console.log(outcome);
+
+    if (outcome.includes("win")) {
+      playerScore++;
+    } else if (outcome.includes("lose")) {
+      computerScore++;
+    } else if (outcome.includes("tie")) {
+      i--;
     }
+
+    if (playerScore === 3 || computerScore === 3) {
+        break;
+    }
+  }
+
+  if (playerScore > computerScore) {
+    console.log(`You won the best of five with a score of ${playerScore}-${computerScore}!`);
+  } else if (computerScore > playerScore) {
+    console.log(`You lost the best of five with a score of ${playerScore}-${computerScore}.`);
+  } else {
+    console.log(`Error. \nPlayer Score: ${playerScore} \nComputer Score: ${computerScore}`);
+  }
 }
